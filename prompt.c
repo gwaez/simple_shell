ce307534f0843672b00a2e6d32c57d346f8111a1
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2023-10-07 12:30:02 -0700

    gw@3z

---
+++ prompt.c
@@ -0,0 +1,10 @@
+#include "shell.h" // Include any necessary header files
+
+/**
+ * display_prompt - Display the shell prompt
+ */
+void display_prompt(void)
+{
+    // Implement the code to display the prompt here
+}
+
---
+++ shell.c
@@ -0,0 +1,12 @@
+#include "shell.h" // Include any necessary header files
+
+/**
+ * execute_command - Execute a command
+ * @command: The user's input command
+ */
+void execute_command(char *command)
+{
+    // Implement the logic to execute the command here
+    // Consider handling built-in commands, PATH, and error cases
+}
+
